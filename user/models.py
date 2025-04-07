2f1620ef25e462e124555b1a0295eec0e7d8c3a1
bereket tesfaye <tesfayebereket909@gmial.com>
2025-04-07 10:36:38 +0300

    authentications and login logout system

--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
